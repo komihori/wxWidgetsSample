31bef05e9fe626e326f3b831fcaf28ff93dc899e
komihori <komihori@kali-komihori.kali-komihori.f5.si>
2025-07-10 11:44:43 +0900

    add Button

--- MyFrame/MyFrame.cpp
+++ MyFrame/MyFrame.cpp
@@ -12,20 +12,28 @@ MyFrame::MyFrame() : wxFrame(nullptr, wxID_ANY, "Sample App") {
 
     vbox  = new wxBoxSizer(wxVERTICAL);
     hbox = new wxBoxSizer(wxHORIZONTAL);
+    hbox2 = new wxBoxSizer(wxHORIZONTAL);
     panel->SetSizer(vbox);
 
     st1 = new wxStaticText(panel, wxID_ANY, "StaticText");
     tc = new wxTextCtrl(panel, wxID_ANY);
+
+    button = new wxButton(panel, wxID_ANY, "Button");
+    button->Bind(wxEVT_BUTTON, &MyFrame::OnClick, this);
     
+
     hbox->Add(st1, 0, wxLEFT);
     hbox->Add(tc, 1);
-    vbox->Add(hbox, 0, wxALIGN_CENTER, 10);
+    vbox->Add(hbox, 0, wxALIGN_LEFT, 10);
+    hbox2->Add(button, 1, wxALIGN_LEFT, 10);
+    vbox->Add(hbox2, 1, wxALIGN_LEFT, 10);
+
     vbox->Add(-1, 10);
 
-    // イベントハンドラーの登録
-    // 自身のメンバー関数を登録する場合：
+    // イベントハンドラーの登録.
+    // 自身のメンバー関数を登録する場合.
     Bind(wxEVT_CLOSE_WINDOW, &MyFrame::OnClose, this);
-    // 無名関数で直接記述する場合：
+    // 無名関数で直接記述する場合.
     Bind(wxEVT_MENU, [this](wxCommandEvent&) { Close(true); }, wxID_EXIT);
 }
 
@@ -33,8 +41,13 @@ MyFrame::~MyFrame() {
 
 }
 
-// wxEVT_CLOSE_WINDOW はウィンドウが閉じられようとしていた場合に呼ばれる。
-// 実装しなかった場合は単に Destroy() が呼ばれる。
+void MyFrame::OnClick(wxCommandEvent &e){
+    wxString tcText = tc->GetValue().ToStdString();
+    st1->SetLabel(tcText);
+}
+
+// wxEVT_CLOSE_WINDOW はウィンドウが閉じられようとしていた場合に呼ばれる.
+// 実装しなかった場合は単に Destroy() が呼ばれる.
 void MyFrame::OnClose(wxCloseEvent& event) {
     Destroy();
 }
--- MyFrame/MyFrame.hpp
+++ MyFrame/MyFrame.hpp
@@ -14,7 +14,11 @@ private:
     wxPanel* panel;
     wxBoxSizer* vbox;
     wxBoxSizer* hbox;
+    wxBoxSizer* hbox2;
     wxStaticText* st1;
     wxTextCtrl* tc;
+    wxButton* button;
+
+    void OnClick(wxCommandEvent&);
 
 };
